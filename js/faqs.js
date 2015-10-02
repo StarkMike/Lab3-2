cdb96d3dd3303f854a37663cfcb284f2a32870be
Mike Stark <mstark@2A140-DVP.conestogac.on.ca>
2015-10-02 15:53:52 -0400

    javascript complete
    toggle function completed.

---
+++ js/faqs.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	// select h2 elements
+	$('#faqs h2').toggle(
+		function() {
+			$(this).toggleClass('minus');
+			$(this).next().show();
+		},
+		function() {
+			$(this).toggleClass('minus');
+			$(this).next().hide();
+		}
+	); // end toggle
+});
